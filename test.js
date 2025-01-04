75f831fac16ad29a9e1b85f444c0b0534c4ad128
Marek1 <marekdadak@seznam.cz>
2025-01-04 13:46:23 +0100

    Pokus o vysledek

---
+++ test.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')();
+console.log("35+23=");
+let result = prompt('Napis spravny vysledek? ');{
+
+    let number = (result === 58);
+    {
+
+    }
+
+    if (result) {
+        console.log("DOBRY vysledek");
+    } else {
+        console.log("Špatny vysledek");
+
+    }
+
+}
